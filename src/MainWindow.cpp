b49633c1580a4ec7f7e372a51bd4ef0bbb73f3e4
Christos Chatzivagias <chatzich@gmail.com>
2018-11-16 00:05:54 +0200

    Skeleton gui 2 bottom listviews

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -1,8 +1,22 @@
 #include "MainWindow.hpp"
+#include <QFrame>
+#include <QDockWidget>
+#include <QListView>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
 {
+	QFrame *mainFrame = new QFrame(this);
+	QDockWidget *dock1 = new QDockWidget(tr("Customers1"), this);
+	QListView *listView1 = new QListView(this);
+	dock1->setWidget(listView1);
+	addDockWidget(Qt::BottomDockWidgetArea, dock1);
+
+	QDockWidget *dock2 = new QDockWidget(tr("Customers2"), this);
+	QListView *listView2 = new QListView(this);
+	dock2->setWidget(listView2);
+	addDockWidget(Qt::BottomDockWidgetArea, dock2);
+	setCentralWidget(mainFrame);
 }
 
 MainWindow::~MainWindow()
